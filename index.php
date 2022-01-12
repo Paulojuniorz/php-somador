a39273e477b81059f957948dc9a0b69ea78a0d82
João Silva <joao@gmail.com>
2022-01-11 22:01:14 -0300

    Adicionados arquivos

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <forms action= 'soma.php' me thod= 'POST'>
+        <input name= 'num1' type="number">
+        <input name= 'num1' type= "number">
+        <button type="submit" >Soma</button>
+</forms>   
+    
+</body>
+</html>
